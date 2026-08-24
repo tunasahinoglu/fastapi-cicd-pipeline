9289f4b6c3455ae081ce95cb0218b02d85f60041
tunasahinoglu <tuna.sahinoglu@sabanciuniv.edu>
2026-08-24 14:22:56 +0300

    EKS cluster creator admin access fix.

--- terraform/infra/eks.tf
+++ terraform/infra/eks.tf
@@ -9,6 +9,7 @@ module "eks" {
   subnet_ids = module.vpc.public_subnets
 
   cluster_endpoint_public_access = true
+  enable_cluster_creator_admin_permissions = true
 
   eks_managed_node_groups = {
     default = {
